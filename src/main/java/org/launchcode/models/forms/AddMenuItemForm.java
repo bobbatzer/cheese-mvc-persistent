b7f73fdbe53e33b1c458cf53da4021f8d1d03f81
bobbatzer <bobbatzer@gmail.com>
2019-04-18 17:21:07 -0500

    Final commit

--- src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
+++ src/main/java/org/launchcode/models/forms/AddMenuItemForm.java
@@ -9,7 +9,7 @@ public class AddMenuItemForm {
 
     private Menu menu;
 
-    private Iterable <Cheese> cheeses;
+    private Iterable<Cheese> cheeses;
 
     @NotNull
     private int menuId;
@@ -17,32 +17,41 @@ public class AddMenuItemForm {
     @NotNull
     private int cheeseId;
 
-    public AddMenuItemForm(Menu menu, Iterable<Cheese> cheeses){
+    public AddMenuItemForm() {
+
+    }
+
+    public AddMenuItemForm(Menu menu, Iterable<Cheese> cheeses) {
         this.menu = menu;
-
         this.cheeses = cheeses;
-    }
-
-
-
-    public int getMenuId() {
-        return menuId;
-    }
-
-    public int getCheeseId() {
-        return cheeseId;
     }
 
     public Menu getMenu() {
         return menu;
     }
 
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
     public Iterable<Cheese> getCheeses() {
         return cheeses;
     }
 
+    public void setCheeses(Iterable<Cheese> cheeses) {
+        this.cheeses = cheeses;
+    }
+
+    public int getMenuId() {
+        return menuId;
+    }
+
     public void setMenuId(int menuId) {
         this.menuId = menuId;
+    }
+
+    public int getCheeseId() {
+        return cheeseId;
     }
 
     public void setCheeseId(int cheeseId) {
